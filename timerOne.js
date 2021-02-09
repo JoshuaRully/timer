3df964858f46c8762b652f852a6d33616fe5ba26
Joshua Rully <joshrully@gmail.com>
2021-02-09 20:44:42 +0000

    fixed usage issues with filter method to check for isNaN and not a negative number

--- timerOne.js
+++ timerOne.js
@@ -1,7 +1,8 @@
-const timers = process.argv.slice(2);
+const timers = process.argv.slice(2).filter(time => !isNaN(time)).filter(time => time >= 0);
+
 
 timers.forEach((time) => {
   setTimeout(() => {
-    process.stdout.write('beep\n');
+    process.stdout.write('beep\n'); //had to use string since system sound would not work
   }, time * 1000);
 });
